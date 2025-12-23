e7049c59f7e1c2c1677eba423e182ccecda742c6
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2025-12-23 14:00:18 +0530

    Time: 42 ms (62.32%) | Memory: 45.4 MB (69.83%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        unordered_map<int, int> prefixSumCount;
+        int prefixSum = 0;
+        int count = 0;
+        prefixSumCount[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            prefixSum += arr[i];
+
+            int remove = prefixSum - k;
+
+            if (prefixSumCount.find(remove) != prefixSumCount.end()) {
+                count += prefixSumCount[remove];
+            }
+
+            prefixSumCount[prefixSum]++;
+        }
+
+        return count;
+    }
+};
