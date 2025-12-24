95b0fd8de62ad0f3c49870866a85d17bfb0f9f40
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2025-12-24 11:42:08 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (61.19%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,17 +2,43 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> um;
+        int count1=0, count2=0;
+        int ele1=0, ele2=0;
         vector<int> ans;
 
-        for (int num : nums) {
-            um[num]++;
+        for(int n: nums){
+            if (count1 == 0 && ele2 != n) {
+                count1 = 1;
+                ele1 = n; 
+            }
+            else if (count2 == 0 && ele1 != n) {
+                count2 = 1;
+                ele2 = n; 
+            } 
+            else if (n == ele1) {
+                count1++;
+            } 
+            else if (n == ele2) {
+                count2++; 
+            } 
+            else {
+                count1--; 
+                count2--;
+            }
         }
 
-        for (auto& pair : um) {
-            if (pair.second > n / 3) {
-                ans.push_back(pair.first);
-            }
+        count1=count2=0;
+        for(int n: nums){
+            if(n==ele1) count1++;
+            if(n==ele2) count2++;
+        }
+
+        int minAppearance = n/3+1;
+        if(count1>=minAppearance){
+            ans.push_back(ele1);
+        }
+        if(count2>=minAppearance && ele1!=ele2){
+            ans.push_back(ele2);
         }
 
         return ans;
