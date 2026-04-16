2c0ae7c60627b910a282864677deb8bedd8b09d1
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2026-04-16 13:50:22 +0530

    Time: 235 ms (39.80%) | Memory: 214 MB (58.71%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -1,55 +1,57 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& arr, int target){
+    int lb(vector<int>& arr, int target){
         int i=0, j=arr.size()-1;
-        while(i <= j){
-            int mid = i+(j-i)/2;
-
-            if(arr[mid] == target) return mid;
-            else if(arr[mid] < target) i = mid+1;
-            else j = mid-1;
+        int ans = arr.size();
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(arr[mid] >= target) {
+                ans = mid;
+                j = mid-1;
+            } else {
+                i = mid+1;
+            }
         }
-        return -1;
+
+        return ans;
     }
 
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        int n = nums.size();
+        int n=nums.size();
         int m = queries.size();
         vector<int> ans(m, -1);
-        unordered_map<int, vector<int>> pos;
-
-        for (int i = 0; i<n; i++) {
-            pos[nums[i]].push_back(i);
+        unordered_map<int, vector<int>> indices;
+        for(int i=0; i<n; i++){
+            indices[nums[i]].push_back(i);
         }
 
         for(int i=0; i<m; i++){
             int q_idx = queries[i];
             int val = nums[q_idx];
+            int res = INT_MAX;
 
-            vector<int>& indices = pos[val];
-            int sz = indices.size();
+            vector<int>& curr_idx = indices[val];
+            int sz = curr_idx.size();
 
-            if (sz == 1) {
+            if(sz == 1){
                 ans[i] = -1;
                 continue;
             }
 
-            int k = binarySearch(indices, q_idx);
-            
-            // if k=0 then k-1=-1 so we add sz
-            int left_idx = indices[(k-1+sz) % sz];
-            int right_idx = indices[(k+1) % sz];
-            
-            int d1 = abs(left_idx - q_idx);
-            int d2 = abs(right_idx - q_idx);
+            int k = lb(curr_idx, q_idx);
 
-            d1 = min(d1, n-d1);
-            d2 = min(d2, n-d2);
+            // The next k at left side (circularly)
+            int left = curr_idx[((k-1)+sz)%sz];
+            int d1 = abs(left-q_idx);
+            res = min({res, d1, n-d1});
 
-            ans[i]= min(d1, d2);
+            // the next k at right side (circularly)
+            int right = curr_idx[(k+1)%sz];
+            int d2 = abs(right-q_idx);
+            res = min({res, d2, n-d2});
 
-
+            ans[i] = res; 
         }
-        return ans;
+    return ans;
     }
 };
