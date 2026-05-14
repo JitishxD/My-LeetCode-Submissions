638f3e6aac566384c7783fd9b7e19a69dff86642
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2026-05-14 13:43:22 +0530

    Time: 17 ms (35.78%) | Memory: 93.4 MB (61.47%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -1,28 +1,46 @@
+// Diffrence Array Technique (DAT)
+// Ref: https://www.youtube.com/watch?v=ZHNVmtm08WY&list=PLpIkg8OmuX-Kqkb8DqDe_4-Tiav6ilS_L&index=2
+// Sol: https://www.youtube.com/watch?v=OzBKFDXNCkY
 class Solution {
 public:
     int minMoves(vector<int>& nums, int limit) {
         int n = nums.size();
-        vector<int> delta((limit << 1) + 2, 0);
 
-        for (int i = 0; i < n >> 1; i++) {
-            int min = nums[i];
-            int max = nums[n - 1 - i];
-            if (min > max) swap(min, max);
+        vector<int> diff(2*limit+2, 0); // or Target Sum array
 
-            delta[2] += 2;
-            delta[min + 1]--;
-            delta[min + max]--;
-            delta[min + max + 1]++;
-            delta[max + limit + 1]++;
+        for (int i=0; i<n/2; i++) {
+
+            int a = nums[i];
+            int b = nums[n-1-i];
+
+            int minVal = min(a, b) + 1;
+            int maxVal = max(a, b) + limit;
+
+            // (2, 2*limit, +2) 
+            diff[2] += 2;
+            diff[2*limit+1] -= 2;
+
+            // for moves = 1
+            // (minVal, maxVal, -1)
+            diff[minVal] += (-1);
+            diff[maxVal+1] -= (-1);
+
+            // for moves = 0 
+            // (a+b, a+b, -1)
+            diff[a+b] += (-1);
+            diff[a+b+1] -= (-1);
         }
 
-        int res = n, moves = 0;
+        int result = INT_MAX;
 
-        for (int targ = 2; targ <= limit * 2; targ++) {
-            moves += delta[targ];
-            res = min(res, moves);
+        // Convert diff into its cumulative form and track the minimum cost
+        // index 0,1 = 0 so they have no effect so start from 2
+        for (int targetSum = 2; targetSum <= 2*limit; targetSum++) {
+            diff[targetSum] += diff[targetSum-1];
+
+            result = min(result, diff[targetSum]);
         }
 
-        return res;
+        return result;
     }
 };
