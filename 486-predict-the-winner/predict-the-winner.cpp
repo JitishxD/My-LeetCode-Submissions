3f9bfed04c06981a1594e180ecec97d3d64ae90b
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2026-08-01 13:15:21 +0530

    Time: 79 ms (26.44%) | Memory: 9.4 MB (99.74%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<int>& nums){
+        if(i>j) return 0;
+        if(i==j) return nums[i];
+
+        int take_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
+        int take_j = nums[j] + min(solve(i+1, j-1, nums), solve(i, j-2, nums));
+
+        return max(take_i, take_j);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int sum = accumulate(begin(nums), end(nums), 0);
+        int p1 = solve(0, nums.size()-1, nums);
+
+        int p2 = sum-p1;
+
+        return true?p1>=p2:false;
+    }
+};
