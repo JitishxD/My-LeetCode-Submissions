b9df7b4b8926e02b676f5a41495075d136edf043
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2026-09-06 18:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.62%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    int binarySearch(const vector<int>& nums, int low, int high, int target, bool searchFirst){
-        int ans=-1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                if(searchFirst) high = mid - 1;
-                else low = mid + 1;
-            } else if (nums[mid] < target) low = mid + 1;
-            else high = mid - 1; 
-        }
-        return ans;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int first = binarySearch(nums, 0, n-1, target, true);
-        if (first == -1) return {-1, -1};
-        int second = binarySearch(nums, 0, n-1, target, false);
+        int j = upper_bound(nums.begin(), nums.end(), target)-nums.begin()-1;
+        int dt = lower_bound(nums.begin(), nums.end(), target)-nums.begin();
 
-        return {first, second};        
+        //prevent out of bound and check if the target is available or not?
+        if (dt == nums.size() || nums[dt] != target) {
+            return {-1, -1};
+        }
+
+        return {dt, j};
     }
 };
