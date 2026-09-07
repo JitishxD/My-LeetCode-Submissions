da749977d0a11906e97919efee4465e2bb455d70
Jitesh Goyal <166510314+JitishxD@users.noreply.github.com>
2026-09-07 21:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (70.87%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,32 +2,36 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return nums[0];
+        int start = 0, end = n-1;
+        int dt = 0;
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
 
-        if (nums[0] != nums[1]) return nums[0];
-        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
-
-        // exclude first and last index
-        int low = 1, high = n - 2;
-        
-        while (low <= high) {
-            int mid = low+(high-low)/2;
-
-            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
-                return nums[mid];
-            }
-
-            // If mid is in the left half (pairing is valid)
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
-                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                low = mid + 1;
-            }
-            // If mid is in the right half (pairing broken earlier)
-            else {
-                high = mid - 1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mid<n-1 && mid>0) {
+                if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid]) {
+                    dt = mid;
+                    break;
+                }
+                
+                if(nums[mid]==nums[mid-1]) {
+                    // in pair we're at second pos
+                    if((mid)%2==0){
+                        // we're at single element's right side
+                        end = mid-1;
+                    } else start = mid+1;
+                } else if (nums[mid]==nums[mid+1]) {
+                    // in pair we're at first pos
+                    if((mid)%2!=0){
+                        // we're at single element's right side
+                        end = mid-1;
+                    } else start = mid+1;
+                }
             }
         }
 
-        return -1;
+        return nums[dt];
     }
 };
